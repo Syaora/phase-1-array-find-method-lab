ff4e6347c50d1a9d61a425a01b0608c90fdb5ea6
Elizabeth Nguyen <saper209@yahoo.com>
2022-04-05 13:04:40 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // code your solution here
+function superbowlWin(obj){
+    const found = obj.find(({result}) => result === 'W')
+    if (found){
+        return found.year
+    } else return undefined
+}
